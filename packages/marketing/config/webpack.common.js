6d345674b9d1d0834ed519b372556a5aad16d9e3
karthikeyana <karthikeyan.a@live.com>
2022-12-29 14:04:55 +0530

    initial commit

---
+++ packages/marketing/config/webpack.common.js
@@ -0,0 +1,25 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|woff|svg|eot|ttf)$/i,
+        use: [{ loader: "file-loader" }],
+      },
+      {
+        test: /\.scss|\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
---
+++ packages/marketing/src/components/Grid.js
@@ -0,0 +1,81 @@
+import React, {
+  useState,
+  useRef,
+  useEffect,
+  useMemo,
+  useCallback,
+} from "react";
+import { AgGridReact } from "ag-grid-react"; // the AG Grid React Component
+import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
+import "ag-grid-community/styles/ag-theme-alpine.css"; // Optional theme CSS
+
+const Grid = () => {
+  const gridRef = useRef(); // Optional - for accessing Grid's API
+  const [rowData, setRowData] = useState([]); // Set rowData to Array of Objects, one Object per Row
+  // Each Column Definition results in one Column.
+  const [columnDefs, setColumnDefs] = useState([
+    { field: "make", filter: true },
+    { field: "model", filter: true },
+    { field: "price" },
+  ]);
+
+  // DefaultColDef sets props common to all Columns
+  const defaultColDef = useMemo(() => ({
+    sortable: true,
+  }));
+
+  // Example of consuming Grid Event
+  const cellClickedListener = useCallback((event) => {
+    console.log("cellClicked", event);
+  }, []);
+
+  // Example load data from sever
+  //   useEffect(() => {
+  //     fetch("https://www.ag-grid.com/example-assets/row-data.json")
+  //       .then((result) => result.json())
+  //       .then((rowData) => setRowData(rowData));
+  //   }, []);
+
+  useEffect(() => {
+    // create interval
+    const interval = setInterval(
+      // set number every 5s
+      () =>
+        setRowData((rowData) => [
+          ...rowData,
+          {
+            make: "Alpha",
+            model: "Apple",
+            price: Math.floor(Math.random() * 100 + 1),
+          },
+        ]),
+      5000
+    );
+
+    // clean up interval on unmount
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div>
+      {/* Example using Grid's API */}
+
+      {/* On div wrapping Grid a) specify theme CSS Class Class and b) sets Grid size */}
+      <div className="ag-theme-alpine" style={{ width: "auto", height: 500 }}>
+        <AgGridReact
+          ref={gridRef} // Ref for accessing Grid's API
+          rowData={rowData} // Row Data for Rows
+          columnDefs={columnDefs} // Column Defs for Columns
+          defaultColDef={defaultColDef} // Default Column Properties
+          animateRows={true} // Optional - set to 'true' to have rows animate when sorted
+          rowSelection="multiple" // Options - allows click selection of rows
+          onCellClicked={cellClickedListener} // Optional - registering for Grid Event
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Grid;
